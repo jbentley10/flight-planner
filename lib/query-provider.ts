4d50a0aa6009f12bd0549e24fbc22082090660a7
John Bentley <jwbentle@gmail.com>
2024-09-07 22:21:19 -0700

    Update lib

---
+++ lib/query-provider.ts
@@ -0,0 +1,10 @@
+import React, { createContext} from 'react';
+
+export const QueryContext = createContext({
+  outbound_date: "",
+  return_date: "",
+  adults: 1,
+  departure_id: "",
+  arrival_id: "",
+  currency: "USD",
+});
