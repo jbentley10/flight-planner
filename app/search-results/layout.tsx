a137572e58ff44ff8ff09de4a70a136b437ec9a9
John Bentley <jwbentle@gmail.com>
2024-09-07 22:20:23 -0700

    Update layout.tsx

--- app/search-results/layout.tsx
+++ app/search-results/layout.tsx
@@ -3,15 +3,6 @@
 import { useFlightSearchStore } from "@/lib/flight-search-store";
 import React, { createContext } from "react";
 
-export const QueryContext = createContext({
-  outbound_date: "",
-  return_date: "",
-  adults: 1,
-  departure_id: "",
-  arrival_id: "",
-  currency: "USD",
-});
-
 function Layout({
   children,
 }: Readonly<{
