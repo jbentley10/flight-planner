cb18542b67fd1032f451ed3878780d6ef17e7983
John Bentley <jwbentle@gmail.com>
2024-09-07 22:26:21 -0700

    Update query-provider.ts

--- lib/query-provider.ts
+++ lib/query-provider.ts
@@ -1,10 +1,10 @@
 import { createContext } from 'react';
 
 export const QueryContext = createContext({
-  outbound_date: "",
-  return_date: "",
+  outbound_date: "2024-09-09",
+  return_date: "2024-09-16",
   adults: 1,
-  departure_id: "",
-  arrival_id: "",
+  departure_id: "PSP",
+  arrival_id: "JFK",
   currency: "USD",
 });
